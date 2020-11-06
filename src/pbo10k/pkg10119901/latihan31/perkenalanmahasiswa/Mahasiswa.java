e9bc0c0ddcd1d70e7598df9b8a7f6ee0a62dd3e6
fauzan_abdussalam <fauzan@3trust.com>
2020-11-07 00:35:10 +0700

    Initial Commit

---
+++ src/pbo10k/pkg10119901/latihan31/perkenalanmahasiswa/Mahasiswa.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k.pkg10119901.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author User
+ */
+public class Mahasiswa 
+{
+    public static void perkenalkanDiri(String nim, String nama)
+    {
+        System.out.println("Hallo Everyone");
+        System.out.println("My NIM is "+nim);
+        System.out.println("My Name is "+nama+"\n");
+    }
+}
---
+++ src/pbo10k/pkg10119901/latihan31/perkenalanmahasiswa/PBO10K10119901Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k.pkg10119901.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author
+ * NAMA     : Fauzan Muhammad Abdussalam
+ * KELAS    : IF-10K
+ * NIM      : 10119901
+ * Deskripsi Program : Menampilkan data perkenalan mahasiswa
+ *
+ */
+public class PBO10K10119901Latihan31PerkenalanMahasiswa {
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa mhs = new Mahasiswa();
+        
+        mhs.perkenalkanDiri("10110269","Rizki Adam Kurniawan");
+        mhs.perkenalkanDiri("10110270","Indra Tiola");
+        mhs.perkenalkanDiri("10110271","Robi Tanzil Ganefi");
+        mhs.perkenalkanDiri("10110272","Muhammad Nur Awaludin");
+    }
+}
